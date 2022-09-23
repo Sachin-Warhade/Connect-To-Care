a259beb22534d9a258042d638417aad4ce91c940
SaurabhKulkarni <saurabhkulkarni654@gmail.com>
2022-09-23 17:00:51 +0530

    Commit 4 from SAURABH-HANUMANT

--- connect-to-care-frontend/src/App.js
+++ connect-to-care-frontend/src/App.js
@@ -10,6 +10,17 @@ import UpdateDoctor from './Components/UpdateDoctor';
 import UpdatePatient from './Components/UpdatePatient';
 import UpdateHospital from './Components/UpdateHospital';
 import PatientAppointment from './Components/PatientAppointment';
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Terms from "./components/Terms";
+import Privacypolicy from "./components/Privacypolicy";
+import Searchhospital from "./components/Searchhospital";
+import Parameter from "./components/Parameter";
+import UpdateParameter from "./components/UpdateParameter";
+import BookAppointment from "./components/BookAppointment";
+import PendingAppointment from "./components/PendingAppointment";
+import PatientAppointmentHistory from "./components/PatientAppointmentHistory";
+import SimpleForm from "./components/SimpleForm";
 
 
 function App() {
@@ -24,9 +35,21 @@ function App() {
             <Route path="/updatepatient" element={<UpdatePatient/>}></Route>
             <Route path="/updatehospital" element={<UpdateHospital/>}></Route>
             <Route path="/PatientAppointment" element={<PatientAppointment/>}></Route>
-            
 
-            </Routes>
+            <Route path="/SimpleForm" element={<SimpleForm />}></Route>
+            <Route path="/About" element={<About />}></Route>
+            <Route path="/Contact" element={<Contact />}></Route>
+
+            <Route path="/Terms" element={<Terms />}></Route>
+            <Route path="/Privacypolicy" element={<Privacypolicy />}></Route>
+            <Route path="/Searchhospital" element={<Searchhospital />}></Route>
+            <Route path="/Parameter" element={<Parameter />}></Route>
+            <Route path="/UpdateParameter" element={<UpdateParameter />}></Route>
+            <Route path="/BookAppointment" element={<BookAppointment />}></Route>
+            <Route path="/PendingAppointment" element={<PendingAppointment />}></Route>
+            <Route path="/PatientAppointmentHistory" element={<PatientAppointmentHistory />}></Route>
+        </Routes>
+      
       </BrowserRouter>
     </div>
   );

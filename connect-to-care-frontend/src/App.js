e01e9a9b43acc2ee4d61db3e9205d5f351464ea4
SaurabhKulkarni <saurabhkulkarni654@gmail.com>
2022-09-23 16:01:40 +0530

    Commit 1 from SAURABH

--- connect-to-care-frontend/src/App.js
+++ connect-to-care-frontend/src/App.js
@@ -1,25 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
 
+import {BrowserRouter, Routes } from 'react-router-dom';
+import{Route} from 'react-router';
+import Login from './Components/Login';
+import AdminLogin from './Components/AdminLogin';
+import ForgotPassword from './Components/ForgotPassword';
+import UpdateDoctor from './Components/UpdateDoctor';
+import UpdatePatient from './Components/UpdatePatient';
+import UpdateHospital from './Components/UpdateHospital';
+import PatientAppointment from './Components/PatientAppointment';
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+          <Routes>
+          <Route path="/Adminlogin" element={<AdminLogin/>}></Route>
+            <Route path="/login" element={<Login/>}></Route>
+            <Route path="/forgotpassword" element={<ForgotPassword/>}></Route>
+            <Route path="/updatedoctor" element={<UpdateDoctor/>}></Route>
+            <Route path="/updatepatient" element={<UpdatePatient/>}></Route>
+            <Route path="/updatehospital" element={<UpdateHospital/>}></Route>
+            <Route path="/PatientAppointment" element={<PatientAppointment/>}></Route>
+            
+
+            </Routes>
+      </BrowserRouter>
     </div>
   );
 }
 
+
+
+     
 export default App;

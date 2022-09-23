e67b6c9dab6693680d01c492f9d89c6f5986b14c
Sachin-Warhade <warhade.sachin09@gmail.com>
2022-09-23 15:36:09 +0530

    Sachin

---
+++ connect-to-care-frontend/src/components/Homepage.js
@@ -0,0 +1,12 @@
+//import { useNavigate } from 'react-router-dom';
+
+function Homepage() {
+
+    return (
+        <div>
+            <h1>Hello CTC</h1>
+        </div>
+    );
+}
+
+export default Homepage;

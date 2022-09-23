e29590daa12f4ad44b26b3ae3d9a8bc1af0b2959
Saikumar <saikumarpadige143@gmail.com>
2022-09-23 15:21:45 +0530

    Commit from saikumar

--- connect-to-care-frontend/src/App.js
+++ connect-to-care-frontend/src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import PatientRegistration from './components/PatientRegistration';
+import AddDoctor from './components/AddDoctor';
+import HospitalRegistration from './components/HospitalRegistration';
+import PatientList from './components/PatientList';
+import HospitalList from './components/HospitalList';
+import DoctorList from './components/DoctorList';
+import HealthTimeline from './components/HealthTimeline';
+import ViewHealthTimeline from './components/ViewHealthTimeline';
+import CreateSlot from './components/CreateSlot';
+import ViewSlots from './components/ViewSlots';
+import UpdateSlot from './components/UpdateSlot';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+          <div className='container'>
+            <Routes>
+            <Route exact path = "/" element = {<PatientRegistration />} />
+            <Route path="/adddoctor" element={<AddDoctor/>}></Route>
+            <Route path="/hospitalregistration" element={<HospitalRegistration/>}></Route>
+            <Route path="/patientlist" element={<PatientList/>}></Route>
+            <Route path="/hospitallist" element={<HospitalList/>}></Route>
+            <Route path="/doctorlist" element={<DoctorList/>}></Route>
+            <Route path="/healthtimeline" element={<HealthTimeline/>}></Route>
+            <Route path="/viewhealthtimeline" element={<ViewHealthTimeline/>}></Route>
+            <Route path="/createslot" element={<CreateSlot/>}></Route>
+            <Route path="/viewslot" element={<ViewSlots/>}></Route>
+            <Route path="/updateslot" element={<UpdateSlot/>}></Route>
+            </Routes>
+          </div>
+      </Router>
     </div>
   );
 }
